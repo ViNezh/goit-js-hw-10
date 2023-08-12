786d177734318400e648904709f3e5ed7579784b
ViNezh <126874232+ViNezh@users.noreply.github.com>
2023-08-12 12:21:40 +0300

    task is finished

--- src/index.js
+++ src/index.js
@@ -1,39 +1,43 @@
-// import { fetchBreeds, fetchCatByBreed } from "./js/cat-api";
+// Імпорт функцій обробки запитів
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
+// Імпорт стилів
 import './css/styles.css';
+// Імпорт бібліотеки повідомлень
 import { Report } from 'notiflix/build/notiflix-report-aio';
+// Імпорт бібліотеки створення списку
 import SlimSelect from 'slim-select-with-styles';
 import 'slim-select-with-styles/dist/slimselect.css';
-import axios from 'axios';
-axios.defaults.headers.common['x-api-key'] =
-  'live_iXXEdjF9QSB0p3CZpEzEqmCEEA1vWX5zCeyafK1rMJs8Zv6o4l9co7BV0kr9bXAJ';
 
-const API_URL = 'https://api.thecatapi.com/v1';
-const API_KEY =
-  'live_iXXEdjF9QSB0p3CZpEzEqmCEEA1vWX5zCeyafK1rMJs8Zv6o4l9co7BV0kr9bXAJ';
-const refs = {
+// Об'єкт посилань на елементи розмітки
+export const refs = {
   selectEl: document.querySelector('.breed-select'),
   loaderEl: document.querySelector('.loader'),
   errorEl: document.querySelector('.error'),
   catList: document.querySelector('.cat-info'),
 };
+// Накладання стилів при завантаженні сторінки
 refs.selectEl.classList.add('is-hidden');
 refs.errorEl.classList.add('is-hidden');
 refs.catList.classList.add('is-hidden');
 refs.loaderEl.classList.replace('loader', 'is-hidden');
-
-function fetchBreeds() {
-  refs.loaderEl.classList.replace('is-hidden', 'loader');
-  return axios.get(`${API_URL}/breeds`);
-}
+// Створюємо масив для зберігання списку порід
 let breedsArr = [];
+// Викликаємо функцію створення списку порід
 fetchBreeds()
   .then(breed => {
+    // Показуємо список і приховуємо анімацію завантаження
     refs.loaderEl.classList.replace('loader', 'is-hidden');
     refs.selectEl.classList.remove('is-hidden');
+    // Наповнюємо масив списком порід
     breed.data.map(element =>
       breedsArr.push({ text: element.name, value: element.id })
     );
-    breedsArr.unshift({ placeholder: true, text: 'Select a cat' });
+    // Додаємо placeholder до списку порід
+    breedsArr.unshift({
+      placeholder: true,
+      text: 'Select the breed of the cat',
+    });
+    // Створюємо спписок порід на сторінці з отриманого масиву
     new SlimSelect({
       select: '.breed-select',
       data: breedsArr,
@@ -41,40 +45,40 @@ fetchBreeds()
         showSearch: false,
       },
     });
+    // Додаємо прослуховувач подій на елемент select
     refs.selectEl.addEventListener('change', onSelected);
   })
+  // Обробка помилки
   .catch(error => {
     onErrorCath();
   });
-//
-//
-function fetchCatByBreed(breedId) {
-  refs.loaderEl.classList.replace('is-hidden', 'loader');
-  refs.catList.classList.add('is-hidden');
-  return axios.get(`${API_URL}/images11/search?breed_ids=${breedId}`);
-}
 
+//Обробка події вибору породи кота
 function onSelected(evt) {
+  // Отримаємо значення id вибраної породи
   const ids = evt.currentTarget.value;
-
+  // Викликаємо функцію обробки запиту
   fetchCatByBreed(ids)
     .then(cat => {
+      // З отриманого масиву вибираємо необхідні дані
       const { breeds } = cat.data[0];
       const url = cat.data[0].url;
-
+      // Створюємо розміту з інформацією про кота
       refs.catList.innerHTML = `
       <div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div>
       <div class="box"><h2>${breeds[0].name}</h2>
       <p>${breeds[0].description}</p>
       <p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
+      // Приховуємо анімацію завантаження і показуємо інформацію про кота
       refs.catList.classList.remove('is-hidden');
       refs.loaderEl.classList.replace('loader', 'is-hidden');
     })
+    // Обробка помилки
     .catch(error => {
       onErrorCath();
     });
 }
-
+// Функція обробки помилки запитів
 function onErrorCath() {
   refs.loaderEl.classList.replace('loader', 'is-hidden');
   refs.selectEl.classList.add('is-hidden');
